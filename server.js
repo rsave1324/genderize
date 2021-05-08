7182f3773df74b86172649a3cd005d3463a488c5
Ubuntu <ubuntu@ip-172-31-14-39.eu-central-1.compute.internal>
2021-05-08 17:28:32 +0000

    bug fixing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const app = express();
 
-app.use('/', express.static('frontend'));
+app.use('/', express.static('src'));
 
 app.use(express.json());
 app.use(express.urlencoded());
