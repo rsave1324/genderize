f403df44d64dcf33ec6211d9fb4fbfc2d4bbc0b5
Ubuntu <ubuntu@ip-172-31-14-39.eu-central-1.compute.internal>
2021-05-09 20:08:17 +0000

    bug fixing

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ app.use('/', express.static('src'));
 app.use(express.json());
 app.use(express.urlencoded());
 
-// Heroku 
-var port = process.env.PORT || 3000;
-// 
-
+var port = 3000
 
 app.listen(port);
